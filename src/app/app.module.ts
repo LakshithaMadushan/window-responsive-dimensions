9b59da95262eaa953a02c62737868abf3c46c05f
LakshithaMadushan <LakshithaMadushan@users.noreply.github.com>
2019-03-15 15:37:18 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ResponsiveDirective } from './directives/responsive.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResponsiveDirective
   ],
   imports: [
     BrowserModule
--- src/app/directives/responsive.directive.ts
+++ src/app/directives/responsive.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import {Directive} from '@angular/core';
+import {HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appResponsive]'
 })
 export class ResponsiveDirective {
 
-  constructor() { }
+  scrHeight: any;
+  scrWidth: any;
+
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event?) {
+    this.scrHeight = window.innerHeight;
+    this.scrWidth = window.innerWidth;
+    console.log(this.scrHeight, this.scrWidth);
+  }
+
+  constructor() {
+    this.getScreenSize();
+  }
 
 }
